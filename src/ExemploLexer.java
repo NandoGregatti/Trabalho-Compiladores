4e6f912637087b0ee96f13ae3080bde68ef47c08
NandoGregatti <fernando.noronha1@estudante.ufla.br>
2023-12-07 13:29:08 -0300

    Analisador Sintatico Implementado

--- src/ExemploLexer.java
+++ src/ExemploLexer.java
@@ -4,24 +4,27 @@ import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.Token;
 import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.tree.ParseTree;
 
 public class ExemploLexer {
     
     public static void main(String[] args) {
-        String filename = "C:\\Users\\ferna\\Desktop\\Trabalho Compiladores\\Trabalho-Compiladores\\ExemploCodigoII.txt";
+        String filename = "C:\\Users\\ferna\\Desktop\\Trabalho Compiladores\\Trabalho-Compiladores\\ExemploCodigoIII.txt";
         
         try {
             CharStream input = CharStreams.fromFileName(filename);
             JavanesLexer lexer =  new JavanesLexer(input);
-            Token token;
-            while (!lexer._hitEOF) {
-                token = lexer.nextToken();
-                System.out.println("Token: " + token.toString());
-                System.out.println("Lexema: " + token.getText());
-                System.out.println("Classe: " + lexer.getVocabulary().getDisplayName(token.getType()));
-            }
+
+            CommonTokenStream tokens = new CommonTokenStream(lexer);
+
+            JavanesParser parser = new JavanesParser(tokens);
+            ParseTree ast = parser.programa();
+
+            System.out.println(ast.toStringTree());
+            
         } catch (IOException e) {
             e.printStackTrace();
         }
+
     }
 }
